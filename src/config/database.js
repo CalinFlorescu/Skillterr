37d79a3c11996cbdacbc8eb691c085c58423b239
Florescu_Calin <calinflorescu3@gmail.com>
2019-10-06 15:37:25 +0300

    implement yarn

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,21 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
-const database = new Sequelize('skillterr_db', 'root', 'Dragonz1997', {
-    host: 'localhost',
-    dialect: 'mysql'
-}, { pool: {
-       max: 5,
-        min: 0,
-        Sequelizeacquire: 30000,
-        idle: 10000
-}});
+const database = new Sequelize(
+  "skillterr_db",
+  "root",
+  "...",
+  {
+    host: "localhost",
+    dialect: "mysql"
+  },
+  {
+    pool: {
+      max: 5,
+      min: 0,
+      Sequelizeacquire: 30000,
+      idle: 10000
+    }
+  }
+);
 
-module.exports =  database;
+module.exports = database;
